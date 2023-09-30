c608f3f5eaa6489b3f99866db4a1388974965325
Anthony Nwokoma <anthonynwokoma1@gmail.com>
2023-10-01 00:46:53 +0100

    Fixed Email Template 7

--- resources/views/emails/email-template.blade.php
+++ resources/views/emails/email-template.blade.php
@@ -5,13 +5,13 @@
 </head>
 <body>
     <div style="background-color:#C8BF8A !important; position:relative;width:94%; padding: 3%;">
-        <img src="{{ env('APP_URL') }}/public/images/{{ $top_image }}" alt="IBOTO EMPIRE">
+        <img src="{{ env('APP_URL') }}/public/images/{{ $top_image }}" alt="IBOTO EMPIRE"  style="width: 100%; height: auto; position: relative;">
         <p>{!! $content !!}</p>
         <div style="float: right">
         <a href="{{ $url }}" style="display: inline-block; padding: 10px; background-color: #007bff; color: #fff; text-decoration: none;">{{$linkText}}</a>
         </div>
         <hr>
-        <img src="{{ env('APP_URL') }}/public/images/{{ $bottom_image }}" alt="IBOTO EMPIRE">
+        <img src="{{ env('APP_URL') }}/public/images/{{ $bottom_image }}" alt="IBOTO EMPIRE"  style="width: 100%; height: auto; position: relative;">
     </div>
 </body>
 </html>

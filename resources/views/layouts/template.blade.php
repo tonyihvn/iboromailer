85b169abab169e6b1486636220241e9d4cc2f5b6
Anthony Nwokoma <anthonynwokoma1@gmail.com>
2023-10-05 10:00:39 +0100

    SOP

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -62,7 +62,7 @@
                     <a href="{{ '/home' }}" class="nav-link">Home</a>
                 </li>
                 <li class="nav-item d-none d-sm-inline-block">
-                    <a href="#" class="nav-link">Contact</a>
+                    <a href="{{url('help')}}" class="nav-link">Help</a>
                 </li>
             </ul>
 
